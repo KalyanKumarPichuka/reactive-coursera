4c8239d9630bc11ca6aff9d9e1fa13b797c67447
Tomás <t.duhourq@gmail.com>
2015-04-23 23:14:58 -0300

    Calculator: 10/10

--- calculator/src/main/scala/calculator/Polynomial.scala
+++ calculator/src/main/scala/calculator/Polynomial.scala
@@ -4,22 +4,18 @@ object Polynomial {
   def computeDelta(a: Signal[Double], b: Signal[Double],
       c: Signal[Double]): Signal[Double] = {
       Signal {
-        val cacheB = b()
-        cacheB * cacheB - 4 * a() * c()
+        b() * b() - 4 * a() * c()
       }
   }
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    val av = a()
-    val bv = b()
-    val cv = c()
     Signal {
-      computeDelta(Signal(av), Signal(bv), Signal(cv))() match {
+      computeDelta(a, b, c)() match {
         case x if x < 0 => Set()
-        case x if x == 0 => Set(-bv / (2 * av))
+        case x if x == 0 => Set(-b() / (2 * a()))
         case x => {
-          Set((-bv + Math.sqrt(x)) / 2 * av, (-bv - Math.sqrt(x)) / 2 * av)
+          Set((-b() + Math.sqrt(x)) / (2 * a()), (-b() - Math.sqrt(x)) / (2 * a()))
         }
       }
     }
